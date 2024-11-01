1208550fe8e82f78623d51e90fed8ba7da3dbca5
Wittmann <ssh.wtmn@gmail.com>
2024-11-01 20:11:58 +0200

    Changed model for connection with database maps name and description

--- src/main/java/com/mainor/project21/glampingestonia/dto/CreateGlampingRequest.java
+++ src/main/java/com/mainor/project21/glampingestonia/dto/CreateGlampingRequest.java
@@ -4,13 +4,15 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class CreateGlampingRequest {
-    private String name;
-    private String description;
     private String county;
     private String linkToBook;
     private List<String> picture;
     private BigDecimal price;
+
+    private Map<String, String> name;
+    private Map<String, String> description;
 }
--- src/main/java/com/mainor/project21/glampingestonia/model/Glamping.java
+++ src/main/java/com/mainor/project21/glampingestonia/model/Glamping.java
@@ -4,15 +4,17 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class Glamping {
     private String id;
-    private String name;
-    private String description;
     private String county;
     private String linkToBook;
     private List<String> picture;
     private BigDecimal price;
+    private Map<String, String> name;
+    private Map<String, String> description;
+
 }
 
